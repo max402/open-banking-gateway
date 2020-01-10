c35b92d24e901ec0e9280061a5779c6c1083bb1f
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-01-10 12:44:47 +0100

    try deploy from develop

--- scripts/deploy_doc.sh
+++ scripts/deploy_doc.sh
@@ -1,38 +1,29 @@
 #!/usr/bin/env bash
 
-if [[ $TRAVIS_REPO_SLUG != "adorsys/open-banking-gateway"
+if [[ $TRAVIS_REPO_SLUG != "max402/open-banking-gateway"
     || $TRAVIS_JDK_VERSION != "openjdk8"
-    || $TRAVIS_PULL_REQUEST != "false"
-    || ! $TRAVIS_TAG ]];
+    || $TRAVIS_PULL_REQUEST != "false" ]];
 then
   echo "ERROR: Documentation deployment for this build not allowed"
   exit 1
 fi
 
-docker run -it --rm -v "$PWD":/src -w /src -u "$(id -u "${USER}"):$(id -g "${USER}")" --env TRAVIS_TAG g0lden/mkdocs make site
+if [ ! "$TRAVIS_TAG" ];
+then
+  TRAVIS_TAG="develop"
+fi
 
 echo -e "Publishing Documentation...\n"
 
-git clone --quiet --branch=gh-pages https://"$GITHUB_TOKEN"@github.com/"$TRAVIS_REPO_SLUG" gh-pages > /dev/null
-cd gh-pages || exit
 
-if [ ! "$TRAVIS_TAG" ];
+if [ "$TRAVIS_TAG" == develop ];
 then
-  rm -Rf ./doc/develop;
-  mkdir -p ./doc/develop && cp -Rf ../site/* ./doc/develop
+  echo "deploy from develop. TRAVIS_TAG=$TRAVIS_TAG"
 else
 (
-  mkdir -p ./doc/"$TRAVIS_TAG" && cp -Rf ../site/* ./doc/"$TRAVIS_TAG"
-  cd doc || exit
-  rm latest
-  ln -s "$TRAVIS_TAG" latest
+  echo "deploy by tag. TRAVIS_TAG=$TRAVIS_TAG"
 )
 fi
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
-git add -f .
-git commit -m "Latest doc on successful travis build $TRAVIS_BUILD_NUMBER for tag $TRAVIS_TAG auto-pushed to gh-pages"
-git push -fq origin gh-pages > /dev/null
 
 echo -e "Published Documentation to gh-pages.\n"
